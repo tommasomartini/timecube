13c5fc2ce112b852a0199d9c1c42cdef31b2c830
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2021-02-15 18:54:13 +0100

    Only accel on, at 10 Hz

--- arduino/libraries/Arduino_LSM9DS1/src/LSM9DS1.h
+++ arduino/libraries/Arduino_LSM9DS1/src/LSM9DS1.h
@@ -28,28 +28,13 @@ class LSM9DS1Class {
     int begin();
     void end();
 
-    // Controls whether a FIFO is continuously filled, or a single reading is stored.
-    // Defaults to one-shot.
-    void setContinuousMode();
-    void setOneShotMode();
+    // Results are in G (Earth gravity).
+    virtual int readAcceleration(float& x, float& y, float& z);
 
-    // Accelerometer
-    virtual int readAcceleration(float& x, float& y, float& z); // Results are in G (earth gravity).
-    virtual int accelerationAvailable(); // Number of samples in the FIFO.
-    virtual float accelerationSampleRate(); // Sampling rate of the sensor.
-
-    // Gyroscope
-    virtual int readGyroscope(float& x, float& y, float& z); // Results are in degrees/second.
-    virtual int gyroscopeAvailable(); // Number of samples in the FIFO.
-    virtual float gyroscopeSampleRate(); // Sampling rate of the sensor.
-
-    // Magnetometer
-    virtual int readMagneticField(float& x, float& y, float& z); // Results are in uT (micro Tesla).
-    virtual int magneticFieldAvailable(); // Number of samples in the FIFO.
-    virtual float magneticFieldSampleRate(); // Sampling rate of the sensor.
+    // Number of samples in the FIFO.
+    virtual int accelerationAvailable();
 
   private:
-    bool continuousMode;
     int readRegister(uint8_t slaveAddress, uint8_t address);
     int readRegisters(uint8_t slaveAddress, uint8_t address, uint8_t* data, size_t length);
     int writeRegister(uint8_t slaveAddress, uint8_t address, uint8_t value);
